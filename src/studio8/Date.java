cfbbbee6163e0dbeb93439c6586ae8c5ccc5645f
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-11-17 12:42:12 -0600

    idk what i did

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,13 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 	private int month;
 	private int day;
 	private int year;
+	private boolean isHoliday;
 	
 	/**
 	 * 
@@ -11,23 +15,50 @@ public class Date {
 	 * @param day
 	 * @param year
 	 */
-	public Date(int month, int day, int year) {
+	public Date(int month, int day, int year, boolean isHoliday) {
 		this.month = month;
 		this.day = day;
 		this.year = year;
+		this.isHoliday = isHoliday;
 	}
 	
 
     @Override
 	public String toString() {
-		return "Date: " + month + "/" + day + "/" + year;
+		return "Date: " + month + "/" + day + "/" + year + " Holiday: " + isHoliday;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
 	}
 
 
 	public static void main(String[] args) {
-    	Date d1 = new Date(12, 11, 2022);
+    	Date d1 = new Date(12, 11, 2022, true);
+    	Date d2 = new Date(12, 12, 2022, false);
+    	Date d3 = new Date(1, 11, 2012, true);
+    	Date d4 = new Date(6, 20, 2002, false);
+    	Date d5 = new Date(1, 21, 2005, true);
     	//d1.toStrin
     	System.out.println(d1);
+    	System.out.println(d1.equals(d2));
+    	
+    	LinkedList<Date> dates = new LinkedList<Date>();
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,22 +1,42 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
-	
+
 	private int hour;
 	private int minute;
-	
+	private boolean isMilitaryTime;
+
 	/**
 	 * 
 	 * @param hour
 	 * @param minute
 	 */
-	public Time(int hour, int minute) {
+	public Time(int hour, int minute, boolean isMilitaryTime) {
 		this.hour = hour;
 		this.minute = minute;
-		
+		this.isMilitaryTime = isMilitaryTime;
+
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
 	/**
 	 * 
 	 * @return hour
@@ -24,7 +44,7 @@ public class Time {
 	public int getHour() {
 		return this.hour;
 	}
-	
+
 	/**
 	 * 
 	 * @return minute
@@ -32,15 +52,15 @@ public class Time {
 	public int getMinute() {
 		return this.minute;
 	}
-	
+
 	public String toString() {
-		return("Time: "+hour+":"+minute);
+		return ("Time: " + hour + ":" + minute + " isMilitaryTime: " + isMilitaryTime);
 	}
 
 	public static void main(String[] args) {
-		Time t1 = new Time(12,24);
-	
+		Time t1 = new Time(12, 24, false);
+
 		System.out.println(t1);
-}
-	
+	}
+
 }
