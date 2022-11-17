0089577e43e6992662464649b8c726d15b5f6589
gracehuang <gracehuang@localhost>
2022-11-17 11:46:04 -0600

    made date constructor

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,7 +1,21 @@
 package studio8;
 
 public class Date {
-
+	private int month;
+	private int day;
+	private int year;
+	
+	/**
+	 * 
+	 * @param month
+	 * @param day
+	 * @param year
+	 */
+	public Date(int month, int day, int year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+	}
 
     public static void main(String[] args) {
 
